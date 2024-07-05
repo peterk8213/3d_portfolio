91f2ec084ee21837ac62f8021e1acac82ac827ac
peterk8213 <peterwilliams8213@gmail.com>
2024-07-05 03:29:48 +0300

    initial commit

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,8 +3,17 @@ import { Html, useProgress } from "@react-three/drei";
 const Loader = () => {
   const { progress } = useProgress();
   return (
-    <Html>
-      <span className="canvas-load"></span>
+    <Html
+      as="div"
+      center
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <span className="canvas-loader"></span>
       <p
         style={{
           fontSize: 14,
